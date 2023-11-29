19f989fd3ed3f1388268cff996f53ad5cf92552e
Onurcan <onrcnernl@gmail.com>
2023-11-29 16:37:40 +0300

    colorscheme previews with telescope now

--- lua/confy/plugins/telescope.lua
+++ lua/confy/plugins/telescope.lua
@@ -2,7 +2,16 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        local telescope = require('telescope')
         local builtin = require('telescope.builtin')
+        telescope.setup {
+            pickers = {
+                colorscheme = {
+                    enable_preview = true
+                }
+            }
+        }
+        vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {})
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
